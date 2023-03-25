f45502bba429d93d02fae12ee12955eea3469c34
ShafiArnob <shafiarnob@gmail.com>
2023-03-25 09:53:38 +0600

    routing

--- learn-laravel/routes/web.php
+++ learn-laravel/routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/demo', function () {
+    return view('demo');
+});
+
+Route::get('/demo/{name}/{id?}', function($name, $id=null){
+    $data = compact('name', 'id');
+    return view('demo')->with($data);
+});
