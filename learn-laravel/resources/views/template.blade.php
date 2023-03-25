a105cad0a1ebf131c50cb92b77e3b42597d3026d
ShafiArnob <shafiarnob@gmail.com>
2023-03-25 12:18:01 +0600

    template - loops

--- learn-laravel/resources/views/template.blade.php
+++ learn-laravel/resources/views/template.blade.php
@@ -16,3 +16,44 @@
 @unless ($name == "Shafi")
   <p>The Name is not Shafi</p>
 @endunless
+
+<!-- looping -->
+<h3>FOR continue break</h3>
+<div style="display:flex; margin:1rem;">
+  @for ($i=1;$i<10; $i++)
+    @if($i==5)
+      @continue
+    @endif
+
+    @if($i==8)
+      @break
+    @endif
+
+    {{$i}}
+  @endfor
+</div>
+
+<h3>WHILE</h3>
+<div style="display:flex; margin:1rem;">
+  @php
+    $i=10;
+  @endphp
+
+  @while ($i<=20)
+    
+    {{$i}}
+    @php $i++; @endphp
+  @endwhile
+</div>
+
+<h3>FOREACH</h3>
+<div style="display:flex; margin:1rem;">
+  @php
+    $arr=[1,2,3,4,5];
+  @endphp
+  <select>
+  @foreach ($arr as $key => $i)
+    <option value="{{$key}}">{{$i}}</option>
+  @endforeach
+  </select>
+</div>
